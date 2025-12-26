6ab7b49b81cb8f5b44af27d2afc4e0a49f18dac5
sushillamsal123 <sushillamsal003@gmail.com>
2025-12-26 18:09:21 +0545

    Add files via upload
    Signed-off-by: sushillamsal123 <sushillamsal003@gmail.com>

---
+++ models/Message.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    sender: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    room: {
+        type: String,
+        default: 'general'
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
